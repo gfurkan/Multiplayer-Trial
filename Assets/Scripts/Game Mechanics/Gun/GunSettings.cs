9c5d9aeba4b6ec4d42fd8ae3a31ad9c3eb1c0d76
furkan <34320035+gfurkan@users.noreply.github.com>
2022-09-19 16:51:55 +0300

    Muzzle flash added.

--- Assets/Scripts/Game Mechanics/Gun/GunSettings.cs
+++ Assets/Scripts/Game Mechanics/Gun/GunSettings.cs
@@ -6,13 +6,19 @@ namespace Gun.Settings
     {
         #region Fields
 
-        [SerializeField] private bool isAutoFireEnabled = false;
-        [SerializeField] private float shootingDelay = 0.15f;
-
+        [SerializeField] private bool _isAutoFireEnabled = false;
+        [SerializeField] private float _shootingDelay = 0.15f;
+        [SerializeField] private GameObject _muzzleFlash;
+        
         #endregion
         
         #region Properties
-        
+
+        public bool isAutoFireEnabled => _isAutoFireEnabled;
+
+        public float shootingDelay => _shootingDelay;
+
+        public GameObject muzzleFlash => _muzzleFlash;
         
         #endregion
 
--- Assets/Scripts/Game Mechanics/Player/PlayerShootController.cs
+++ Assets/Scripts/Game Mechanics/Player/PlayerShootController.cs
@@ -1,5 +1,7 @@
 using System;
+using Gun.Settings;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Player.Shoot
 {
@@ -8,11 +10,18 @@ namespace Player.Shoot
         #region Fields
 
         [SerializeField] private GameObject bulletImpactPrefab;
-        [SerializeField] private float shootingDelay = 0;
+        [SerializeField] private float shootingDelay = 0,muzzleCounter=0;
+        [SerializeField] private GunSettings[] guns;
         
         private Camera cam;
         private float time = 0;
-
+        private float flashRotationY = 0;
+            
+        private int gunIndex = 0;
+        private GunSettings activeGun;
+        
+        float tempValue = 0;
+        private bool isAutoFireEnabled = false;
         #endregion
         
         #region Properties
@@ -26,31 +35,71 @@ namespace Player.Shoot
         {
             cam = Camera.main;
             time = shootingDelay;
+            tempValue = muzzleCounter;
+            
+            ChangeGun(0);
         }
         
         void Update()
         {
-            if (Input.GetButton("Fire1"))
-            { 
-                time += Time.deltaTime;
-                
-                if (time > shootingDelay)
-                { 
+            DisableMuzzleFlash();
+            
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!isAutoFireEnabled)
+                {
                     Shoot(); 
-                    time = 0;
+                }
+            }
+            
+            if (Input.GetButton("Fire1"))
+            {
+                if (isAutoFireEnabled)
+                {
+                    time += Time.deltaTime;
+                    if (time > shootingDelay)
+                    { 
+                        Shoot(); 
+                        time = 0;
+                    }
                 }
             }
 
             if (Input.GetButtonUp("Fire1"))
             {
                 time = shootingDelay;
+                activeGun.muzzleFlash.SetActive(false);
+            }
+
+            if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+            {
+                gunIndex++;
+                if (gunIndex > guns.Length-1)
+                {
+                    gunIndex = 0;
+                }
+
+                ChangeGun(gunIndex);
+            }
+            else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+            {
+                gunIndex--;
+                if (gunIndex <0)
+                {
+                    gunIndex = guns.Length-1;
+                }
+
+                ChangeGun(gunIndex);
             }
         }
 
         private void OnDrawGizmos()
         {
-            Debug.DrawRay(cam.transform.position,CalculateShootingDirection().direction*10);
             Gizmos.color=Color.red;
+            if (cam != null)
+            {
+                Debug.DrawRay(cam.transform.position,CalculateShootingDirection().direction*10);
+            }
         }
 
         #endregion
@@ -60,9 +109,11 @@ namespace Player.Shoot
         private void Shoot()
         {
             RaycastHit raycastHit;
+
             if (Physics.Raycast(CalculateShootingDirection(), out raycastHit,100))
             {
                 CreateImpact(raycastHit.point,raycastHit.normal);
+                activeGun.muzzleFlash.SetActive(true);
             }
         }
         private Ray CalculateShootingDirection()
@@ -76,6 +127,34 @@ namespace Player.Shoot
         {
             var bulletImpact=Instantiate(bulletImpactPrefab, hitPosition+(hitNormal*0.002f), Quaternion.LookRotation(hitNormal, Vector3.up));
             Destroy(bulletImpact,2);
+        }
+
+        void ChangeGun(int index)
+        {
+            if (activeGun != null)
+            {
+                activeGun.gameObject.SetActive(false);
+            }
+
+            activeGun = guns[index];
+            shootingDelay = activeGun.shootingDelay;
+            isAutoFireEnabled = activeGun.isAutoFireEnabled;
+            
+            activeGun.gameObject.SetActive(true);
+        }
+
+        void DisableMuzzleFlash()
+        {
+            if (activeGun.muzzleFlash.activeInHierarchy)
+            {
+                muzzleCounter -= Time.deltaTime;
+                
+                if (muzzleCounter <= 0)
+                {
+                    activeGun.muzzleFlash.SetActive(false);
+                    muzzleCounter = tempValue;
+                }
+            }
         }
         #endregion
 
